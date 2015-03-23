bf37be2694b30c92260e4f2053b2c553961e8041
Shinsuke Nishio <nishio@densan-labs.net>
2015-03-24 02:07:09 +0900

    Carrierwave ファイルアップロードの基礎サンプルを追加

---
+++ upload/carrierwave_file/app/controllers/products_controller.rb
@@ -0,0 +1,45 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product, notice: 'Product was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :image, :image_cache, :remove_image)
+  end
+end
